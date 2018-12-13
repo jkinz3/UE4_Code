57785da15ada5512d4c5022265e6123ae54e79c0
Kevin <eckstein.kevin@outlook.com>
2018-12-13 15:20:48 -0600

    deleted reference to failed movement component I gave up on

--- Private/FPCharacter.cpp
+++ Private/FPCharacter.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "../Public/FPCharacter.h"
-#include "FPCharacterMovementComponent.h"
 #include "Engine.h"
 
 // Sets default values
